edeadf91219421dc95e3b953eb3093720c03b8c5
MelvinjoseC <165891174+MelvinjoseC@users.noreply.github.com>
2026-08-13 14:21:43 +0530

    config: load Django SECRET_KEY from environment variables

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import pymysql
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hg(x496btninua9c7ptd+4_3_7-l_kvc%g3wng6%bfg^u986*#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-hg(x496btninua9c7ptd+4_3_7-l_kvc%g3wng6%bfg^u986*#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
